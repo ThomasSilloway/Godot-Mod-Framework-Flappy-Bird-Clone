8295f74b3033b1c455dfd8b50d85c4305e4934d3
Thomas Silloway <tcsillow@gmail.com>
2025-03-05 18:06:56 -0500

    Fix startup.bat to work properly with the new startup project
    Used to be called main

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,8 +1,18 @@
 @echo off
 setlocal
 
+:: Get the script's directory
+set "script_dir=%~dp0"
+pushd %script_dir%..
+set "workspace_root=%cd%"
+popd
+
 :: Define the base build directory
-set build_path=C:\GithubRepos\GodotModCapableGame\build\
+set build_path=%workspace_root%\build\
+
+echo Current script directory: %script_dir%
+echo Workspace path: %workspace_root%
+echo Builds folder path: %build_path%
 
 :: Find the latest build folder by sorting directories by name (since they're datetime-based)
 :: Skip the mods folder by adding a findstr filter
@@ -17,7 +27,7 @@ if not defined latest_build (
     exit /b 1
 )
 
-set run_path_file=main.console.exe
+set run_path_file=startup.console.exe
 set full_path=%build_path%%latest_build%\%run_path_file%
 
 :: Check if the executable exists
